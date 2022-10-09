367001e9e3eba7a092d2d3d35ca7ee23aae78906
lachiefish <lachiefish@gmail.com>
2022-10-09 13:34:58 +1100

    Updated to work with Fi Light BLE lights
    Changed byte codes for reading and writing

--- triones.py
+++ triones.py
@@ -5,14 +5,14 @@ import asyncio
 
 from .const import LOGGER
 
-WRITE_CHARACTERISTIC_UUIDS = ["0000ffd5-0000-1000-8000-00805f9b34fb", "0000ffd9-0000-1000-8000-00805f9b34fb", "0000ffe5-0000-1000-8000-00805f9b34fb", "0000ffe9-0000-1000-8000-00805f9b34fb"]
-READ_CHARACTERISTIC_UUIDS  = ["0000ffd0-0000-1000-8000-00805f9b34fb", "0000ffd4-0000-1000-8000-00805f9b34fb", "0000ffe0-0000-1000-8000-00805f9b34fb", "0000ffe4-0000-1000-8000-00805f9b34fb"]
+WRITE_CHARACTERISTIC_UUIDS = ["0000ffc1-0000-1000-8000-00805f9b34fb", "0000ffd5-0000-1000-8000-00805f9b34fb", "0000ffd9-0000-1000-8000-00805f9b34fb", "0000ffe5-0000-1000-8000-00805f9b34fb", "0000ffe9-0000-1000-8000-00805f9b34fb"]
+READ_CHARACTERISTIC_UUIDS  = ["0000ffc2-0000-1000-8000-00805f9b34fb", "0000ffd0-0000-1000-8000-00805f9b34fb", "0000ffd4-0000-1000-8000-00805f9b34fb", "0000ffe0-0000-1000-8000-00805f9b34fb", "0000ffe4-0000-1000-8000-00805f9b34fb"]
 
 async def discover():
     """Discover Bluetooth LE devices."""
     devices = await BleakScanner.discover()
     LOGGER.debug("Discovered devices: %s", [{"address": device.address, "name": device.name} for device in devices])
-    return [device for device in devices if device.name.lower().startswith("triones") or device.name.lower().startswith("ledble")]
+    return [device for device in devices if device.name.lower().startswith("magic")]
 
 def create_status_callback(future: asyncio.Future):
     def callback(sender: int, data: bytearray):
@@ -52,16 +52,16 @@ class TrionesInstance:
 
     async def set_color(self, rgb: Tuple[int, int, int]):
         r, g, b = rgb
-        await self._write([0x56, r, g, b, 0x00, 0xF0, 0xAA])
+        await self._write([0x28, r, g, b,0x00, 0x00, 0x00, 0xF0, 0x29])
     
     async def set_white(self, intensity: int):
-        await self._write([0x56, 0, 0, 0, intensity, 0x0F, 0xAA])
+        await self._write([0x28, 0, 0, 0xFD, intensity, intensity, 0x00, 0x0F, 0x29])
 
     async def turn_on(self):
-        await self._write(bytearray([0xCC, 0x23, 0x33]))
+        await self._write(bytearray([0xFB, 0xF0, 0xFA]))
         
     async def turn_off(self):
-        await self._write(bytearray([0xCC, 0x24, 0x33]))
+        await self._write(bytearray([0xFB, 0x0F, 0xFA]))
     
     async def update(self):
         try:
@@ -85,14 +85,14 @@ class TrionesInstance:
 
             future = asyncio.get_event_loop().create_future()
             await self._device.start_notify(self._read_uuid, create_status_callback(future))
-            await self._write(bytearray([0xEF, 0x01, 0x77]))
+            await self._write(bytearray([0xBB, 0xF0, 0xCC]))
             
             await asyncio.wait_for(future, 5.0)
             await self._device.stop_notify(self._read_uuid)
             
             res = future.result()
-            self._is_on = True if res[2] == 0x23 else False if res[2] == 0x24 else None
-            self._rgb_color = (res[6], res[7], res[8])
+            self._is_on = True if res[2] == 0xf0 else False if res[2] == 0x0f else None
+            self._rgb_color = (res[5], res[6], res[7])
             self._brightness = res[9] if res[9] > 0 else None
             LOGGER.debug(''.join(format(x, ' 03x') for x in res))
 
